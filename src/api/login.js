74032f0abebc0bdc922467b6567426dbf06a8e86
jichuangsi <school@jichuangsi>
2018-12-11 11:19:31 +0800

    [jiewen.wu]更新登录接口

--- src/api/login.js
+++ src/api/login.js
@@ -2,18 +2,18 @@ import axios from '../utils/axios'
 
 const apiUrl = 'http://school.jichuangsi.com:81/api/auth/login';
 
-export async function login(userId, pwd) {
+export async function login(userAccount, userPwd) {
     let res = await axios({
         method: 'POST',
         url: `${apiUrl}`,
         data: {
-            userId,
-            pwd
+            userAccount,
+            userPwd
         }
     });
     if (res.data.code === '0010') {
         localStorage.setItem("token", res.data.data.accessToken);
-        return res.data.data ? res.data.data.userBaseInfo : false;
+        return res.data.data ? res.data.data.user : false;
     } else {
         throw res.data.msg;
     }
