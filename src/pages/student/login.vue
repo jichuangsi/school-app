a9b2d5daf435f0ac39402972ffe062a110ba0624
jichuangsi <school@jichuangsi>
2019-05-31 20:32:48 +0800

    [jiewen.wu]fix bug

--- src/pages/student/login.vue
+++ src/pages/student/login.vue
@@ -84,9 +84,9 @@ export default {
               path: this.$store.state.userroute
             });
           } else {
-            if (userInfo.portrait != null) {
+            /*if (userInfo.portrait != null) {
               let a = await getImg(userInfo.portrait);
-            }
+            }*/
             if (userInfo) {
               this.flag = true;
               this.$router.push({
